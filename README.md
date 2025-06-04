c70f11ae34e8526da07bd8c79d82baf02bc21037
Wyco <herik.dev06@gmail.com>
2025-06-04 14:15:20 +0700

    layout

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Day 1 - install shadcn/ui library
         npx shadcn@latest init
+
+        add button component
+        npx shadcn@latest add button
+
+        Layout
+        edit Header.js, Sidebar.js, page.js and layout.js
---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default async function Header() {
+  return (
+    <nav className="flex justify-between items-center border rounded border-slate-500 px-4 py-2 my-4">
+      <h1 cassName="font-bold text-xl">
+        <Link href="/" className="font-bold text-lg">
+          Next Movies
+        </Link>
+      </h1>
+    </nav>
+  );
+}
